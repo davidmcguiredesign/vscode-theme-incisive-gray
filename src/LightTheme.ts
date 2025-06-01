f81778a8b8281d85c62f9f77c00a1f56d770f921
David McGuire <davidmcguiredesign@gmail.com>
2025-05-31 17:55:21 -0700

    less purpley, using HSLuv

--- src/LightTheme.ts
+++ src/LightTheme.ts
@@ -7,9 +7,9 @@ export class LightTheme extends BaseThemeLight {
   colorFG = this.hsl(0, 0, 20);
 
   colorSubtle = this.hsl(320, 40, 40);
-  colorUno = this.hsl(320, 95, 35);
-  colorDue = this.hsl(215, 95, 35);
-  colorTre = this.hsl(125, 95, 22);
+  colorUno = this.hsl(328, 100, 38);
+  colorDue = this.hsl(222, 88, 43);
+  colorTre = this.hsl(120, 100, 22);
 
   colorBorder0 = this.hsl(0, 0, 70);
   colorBorder1 = this.hsl(0, 0, 40);
