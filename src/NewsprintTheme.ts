52d521c3f630b6f4373d158becaee32408fd65f7
Gage <aegw7e1c5wpwz9w@gmail.com>
2026-01-25 17:40:07 -0800

    level fine-tuning

--- src/NewsprintTheme.ts
+++ src/NewsprintTheme.ts
@@ -3,7 +3,7 @@ import { BaseThemeLight } from "./Theme";
 export class NewsprintTheme extends BaseThemeLight {
   colorBG0 = this.hsl(60, 0, 83);
   colorBG1 = this.hsl(0, 0, 79);
-  colorBG2 = this.hsl(0, 0, 73);
+  colorBG2 = this.hsl(0, 0, 74);
   colorFG = this.hsl(0, 0, 0);
 
   colorSubtle = this.hsl(322, 10, 49);
@@ -11,9 +11,9 @@ export class NewsprintTheme extends BaseThemeLight {
   colorDue = this.hsl(215, 100, 33);
   colorTre = this.hsl(125, 100, 20);
 
-  colorBorder0 = this.hsl(0, 0, 60);
+  colorBorder0 = this.hsl(0, 0, 50);
   colorBorder1 = this.hsl(0, 0, 20);
   colorStatusBG = this.colorFG;
   colorStatusFG = this.colorBG0;
-  colorWidgetBG = this.lighten(this.colorBG0, 4);
+  colorWidgetBG = this.colorBG2;
 }
